133550d01b96936b829323b336d122f6d21c1ef1
Carolina Gota <88778272+CaroGota@users.noreply.github.com>
2025-11-03 18:22:24 -0300

    Update Empleado.java

--- src/implementacion/Empleado.java
+++ src/implementacion/Empleado.java
@@ -1,33 +1,39 @@
-package implementacion;
+package entidades;
 
 public abstract class Empleado {
-//-------------------------------------------------------
-//para tipo abstracto, los atributos deben usar protected
-//-------------------------------------------------------
-	protected String nombre;
-	protected Integer legajo;
-	protected Integer retrasos;
-	
-	public Empleado (String nombre, Integer legajo) {
-		this.nombre = nombre;
-		this.legajo = legajo;
-		this.retrasos = 0; 		//cualquier empleado nuevo empieza con 0 retrasos
+	//-------------------------------------------------------
+	//para tipo abstracto, los atributos deben usar protected
+	//-------------------------------------------------------
+		protected String nombre;
+		protected Integer legajo;
+		protected Integer retrasos;
+		protected boolean sinTareaAsiganda;
+	    protected Boolean estaDisponible;
 
-	}
-	//----------------------------------------------------------------------------------------------------
-	//----- Empleado deberia tener un metodo abstracto para que sus hijos tengan que calcular el costo de 
-	//----- una nueva Tarea y cada hijo lo calcula a su manera (Polimorfismo)
-	//----------------------------------------------------------------------------------------------------
-	public abstract Double calcularCostoTarea(Double diasNecesarios); 
-	//su único propósito es forzar a las clases hijas 
-	//a que ellas escriban el contenido.
-	
-	public void registrarRetraso() {
-		this.retrasos++; //un retraso aumenta en "1"
-	}
-	
-	//getters
-	public String getNombre() {return this.nombre;}
-	public Integer getLegajo() {return this.legajo;}
-	public Integer getRetrasos() {return this.retrasos;}
+		
+		public Empleado (String nombre, Integer legajo) {
+			this.nombre = nombre;
+			this.legajo = legajo;
+			this.retrasos = 0; 		//cualquier empleado nuevo empieza con 0 retrasos
+			this.sinTareaAsiganda = false;
+	        this.estaDisponible = false;
+
+		}
+		//----------------------------------------------------------------------------------------------------
+		//----- Empleado deberia tener un metodo abstracto para que sus hijos tengan que calcular el costo de 
+		//----- una nueva Tarea y cada hijo lo calcula a su manera (Polimorfismo)
+		//----------------------------------------------------------------------------------------------------
+		
+		public abstract Double calcularCostoTarea(Double diasNecesarios); 
+		//su único propósito es forzar a las clases hijas 
+		//a que ellas escriban el contenido.
+		
+		public void registrarRetraso() {
+			this.retrasos++; //un retraso aumenta en "1"
+		}
+		
+		//getters
+		public String getNombre() {return this.nombre;}
+		public Integer getLegajo() {return this.legajo;}
+		public Integer getRetrasos() {return this.retrasos;}
 }
